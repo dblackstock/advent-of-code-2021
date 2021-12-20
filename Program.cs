948648e635e7a988f957f9297f8b0ec52e754491
Davey Blackstock <davey.blackstock@emishealth.com>
2021-12-20 15:35:30 +0000

    day 1 complete

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace advent_of_code_2021
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// manually update which function is being called
+			// in order to solve various challenges
+			DepthCheck.SonarSweep();
+		}
+	}
+}
