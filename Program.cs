b4644f848414a080610a42df34e26dc5f180f23e
Davey Blackstock <davey.blackstock@emishealth.com>
2021-12-21 10:39:57 +0000

    day 3 complete - system to run tests improved

--- Program.cs
+++ Program.cs
@@ -8,7 +8,24 @@ namespace advent_of_code_2021
 		{
 			// manually update which function is being called
 			// in order to solve various challenges
-			Dive.Navigation();
+			Console.WriteLine("Which day do you want to see output for?");
+			string day = Console.ReadLine();
+
+			switch (day)
+			{
+				case "1":
+					DepthCheck.SonarSweep();
+					break;
+				case "2":
+					Dive.Navigation();
+					break;
+				case "3":
+					BinaryDiagnostic.Diagnostic();
+					break;
+				default:
+					Console.WriteLine("Day not found. Please enter a value from 1 to 25. Perhaps your chosen value has not been added yet.");
+					break;
+			}
 		}
 	}
 }
